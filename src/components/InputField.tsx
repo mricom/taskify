21b9e0af5ade0dda46ce937f8d6c197c00335e2c
mricom <mricomorillo@gmail.com>
2022-10-13 15:08:30 +0200

    Created input conponent and styled it.

---
+++ src/components/InputField.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+const InputField = () => {
+  return <form className="input">
+    <input type="input" placeholder="Enter a task" className="input__box"></input>
+    <button className="input__submit" type="submit">
+      Go
+    </button>
+
+  </form>;
+};
+
+export default InputField;
